9be48ce0bda48d48627cdaf639324986dc75454c
Lisandro Lanfranco <contacto@lisandrolanfranco.com>
2015-05-18 16:10:36 -0300

    Creación del fichero
    Lección sobre Listas y como recorrerlas con un bucle For

--- 002_meses.py
+++ 002_meses.py
@@ -1,1 +1,5 @@
 Meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+DiasPorMes = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+for i in range (0,12):
+  print '%s tiene %d dias.' % (Meses[i], DiasPorMes[i])
