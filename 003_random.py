f67f2989832fb0c8d48a58e38b3c051f35100387
Lisandro Lanfranco <contacto@lisandrolanfranco.com>
2015-05-20 13:21:38 -0300

    Creacion del fichero
    Ejemplo de inclusion de un modulo y uso de la funcion randint para generar enteros aleatorios.

---
+++ 003_random.py
@@ -0,0 +1,9 @@
+#===============================================
+# 003_random.py
+# Ejemplo de como incluir un módulo.
+#===============================================
+import random
+
+#Imprime 14 números enteros aleatorios
+for i in range (1,15):
+  print random.randint(1,10)
