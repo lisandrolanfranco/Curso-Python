54bc57208e3598eaafc429e4d9981fdd71576535
Lisandro Lanfranco <contacto@lisandrolanfranco.com>
2015-05-20 13:35:35 -0300

    Creación del fichero
    Uso de funciones

---
+++ 004_funciones.py
@@ -0,0 +1,8 @@
+def DoTwo(num1, num2):
+  print 'Suma de dos numeros %d y %d = %d ' % (num1, num2, num1+num2)
+  print 'Multiplicacion de dos numeros %d y %d = %d ' % (num1, num2, num1*num2)
+  print 'Resta de dos numeros %d y %d = %d ' % (num1, num2, num1-num2)
+  print '\n'
+  
+DoTwo(1,2)
+DoTwo(10,5)
